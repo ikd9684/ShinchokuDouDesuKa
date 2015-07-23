a2a1db5ba599cadb5a71b57e5e70be681a10651e
ikd9684 <ikd9684@gmail.com>
2015-07-23 21:18:14 +0900

    できたよ。

--- ShinchokuDouDesuKa/Program.cs
+++ ShinchokuDouDesuKa/Program.cs
@@ -4,12 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShinchokuDouDesuKa
+namespace jp._9684.sandbox.SinchokuDouDesuKa
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                mainProcess();
+            }
+            catch (Exception e)
+            {
+                Exception ie = e;
+                do
+                {
+                    Console.WriteLine(ie);
+                    ie = ie.InnerException;
+                }
+                while (ie != null);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("何かキーを押してください。");
+                Console.ReadKey();
+            }
         }
+
+        static void mainProcess()
+        {
+            string[] words
+                = {
+                      "進捗", "どう", "です", "か",
+                  };
+
+            int totalWordCount = 0;
+            int matchNumber = 0;
+            Random rnd = new Random();
+
+            while (matchNumber < words.Length)
+            {
+                string word = words[rnd.Next(4)];
+                Console.Write(word);
+                totalWordCount += word.Length;
+                matchNumber = (word == words[matchNumber]) ? matchNumber + 1 : 0;
+            }
+
+            Console.WriteLine("？？？");
+            Console.WriteLine("{0}文字で煽られました。", totalWordCount);
+        }
+
     }
 }
